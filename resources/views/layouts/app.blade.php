d4590ab390b9cf26329f18be59db57a10f84a959
Sr. Croy <rubiocr007@gmail.com>
2024-11-07 08:05:31 -0600

    Arreglando estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,75 +11,86 @@
     @stack('css')
 </head>
 <body>  
-    <header>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/principal">Tienda Romero</a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Estados Financieros
-                                </a>
-                                <ul class="dropdown-menu">
-                                    @if (session('user')->tblPermiso->consultarCuentasT)
-                                        <li><a class="dropdown-item" href="#">Cuentas T</a></li>
-                                    @endif
-                                    @if (session('user')->tblPermiso->consultarEstadosFinancieros)
-                                        <li><a class="dropdown-item" href="/balancegeneral">Balance General</a></li>
-                                        <li><a class="dropdown-item" href="/estadoresultado">Estado De Resultados</a></li>
-                                        <li><a class="dropdown-item" href="/estadocapital">Estado De Capital</a></li>
-                                        <li><a class="dropdown-item" href="{{route('bal_comprobacion.index')}}">Balance De Comprobacion</a></li>
-                                    @endif
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Asientos Diarios
-                                </a>
-                                <ul class="dropdown-menu">
-                                    @if (session('user')->tblPermiso->ingresarRegistroDiario)
-                                        <li><a class="dropdown-item"  href="{{route('tbl-registro-diario.create')}}">Ingresar Asiento Diario</a></li>
-                                    @endif
-                                    @if (session('user')->tblPermiso->consultarRegistroDiario)
-                                        <li><a class="dropdown-item" href="{{route('tbl-registro-diario.index')}}">Consultar Asientos Diarios</a></li>
-                                    @endif
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Administración
-                                </a>
-                                <ul class="dropdown-menu">
-                                    @if (session('user')->tblPermiso->crearUsuarios)
-                                        <li><a class="dropdown-item" href="{{route('users.index')}}">Usuarios</a></li>
-                                    @endif
-                                    @if (session('user')->tblPermiso->gestionarPermisos)
-                                        <li><a class="dropdown-item" href="{{route('tbl-permisos.index')}}">Permisos</a></li>
-                                    @endif
-                                    <li><a class="dropdown-item" href="{{route('tbl-cuentas.index')}}">Cuentas</a></li>
-
-                                </ul>
-                            </li>                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}">Cerrar Sesión</a>
-                            </li>
-                      
-
-                    </ul>
-                </div>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/principal">
+                <span class="text-warning">Tienda </span><span class="text-dark">Romero</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <!-- Dropdown Estados Financieros -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Estados Financieros
+                        </a>
+                        <ul class="dropdown-menu">
+                            @if (session('user')->tblPermiso->consultarCuentasT)
+                                <li><a class="dropdown-item" href="#">Cuentas T</a></li>
+                            @endif
+                            @if (session('user')->tblPermiso->consultarEstadosFinancieros)
+                                <li><a class="dropdown-item" href="/balancegeneral">Balance General</a></li>
+                                <li><a class="dropdown-item" href="/estadoresultado">Estado De Resultados</a></li>
+                                <li><a class="dropdown-item" href="/estadocapital">Estado De Capital</a></li>
+                                <li><a class="dropdown-item" href="{{route('bal_comprobacion.index')}}">Balance De Comprobacion</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                    <!-- Dropdown Asientos Diarios -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Asientos Diarios
+                        </a>
+                        <ul class="dropdown-menu">
+                            @if (session('user')->tblPermiso->ingresarRegistroDiario)
+                                <li><a class="dropdown-item" href="{{route('tbl-registro-diario.create')}}">Ingresar Asiento Diario</a></li>
+                            @endif
+                            @if (session('user')->tblPermiso->consultarRegistroDiario)
+                                <li><a class="dropdown-item" href="{{route('tbl-registro-diario.index')}}">Consultar Asientos Diarios</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                    <!-- Dropdown Administración -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Administración
+                        </a>
+                        <ul class="dropdown-menu">
+                            @if (session('user')->tblPermiso->crearUsuarios)
+                                <li><a class="dropdown-item" href="{{route('users.index')}}">Usuarios</a></li>
+                            @endif
+                            @if (session('user')->tblPermiso->gestionarPermisos)
+                                <li><a class="dropdown-item" href="{{route('tbl-permisos.index')}}">Permisos</a></li>
+                            @endif
+                            <li><a class="dropdown-item" href="{{route('tbl-cuentas.index')}}">Cuentas</a></li>
+                        </ul>
+                    </li>
+                    <!-- Cerrar Sesión -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}">Cerrar Sesión</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
+</header>
+
+
     
     <div class="container content @yield('extra-classes')" style="@yield('estilo-classes')">
         @yield('content')
     </div>
 
-    <footer class="text-center py-4">
-        <p>&copy; 2024 Mi Sitio Web. Todos los derechos reservados.</p>
-    </footer>
+    <!-- footer starts -->
+    <footer class="bg-dark p-2 text-center">
+          <div class="container">
+              <p class="text-white">All Right Reserved By @website Name</p>
+          </div>
+      </footer>
+      <!-- footer ends -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
